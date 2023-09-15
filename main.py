ac246b51af2d54f64172129e49847a42e325a0b4
Swoyam Thapa <thapaswoyam123@gmail.com>
2023-09-14 21:53:51 -0500

    website iniciated

--- main.py
+++ main.py
@@ -1,56 +1,60 @@
-import currency_name
-import conversion
-import currency_conversion
+from website import create_app
+from modules.currency_name import Currency_name
+from modules.conversion import Conversion
+from modules.currency_conversion import Currency_conversion
 
-def main():
 
-    country1 = input("Enter your first country:")
-    country2 = input("Enter the second country: ")
-    amount = float(input("Enter the amount you want to transfer: "))
 
-    _curr1 = currency_name.Currency_name(country1)
-    _curr2 = currency_name.Currency_name(country2)
+# country1 = input("Enter your first country:")
+# country2 = input("Enter the second country: ")
+# amount = float(input("Enter the amount you want to convert: "))
 
-    curr1 = _curr1.currency()
-    curr2 = _curr2.currency()
+# _curr1 = Currency_name(country1)
+# _curr2 = Currency_name(country2)
 
-    _conversion = currency_conversion.Currency_conversion(curr1,curr2,amount)
-    _convert = _conversion.convert()
+# curr1 = _curr1.currency()
+# curr2 = _curr2.currency()
 
-    if(curr1 == "not found" or curr2 == "not found"):
-        print(f"{country1} and {country2} not found.")
-        print("Enter again: ")
-        print()
-        main()
-        
+# _conversion = Currency_conversion(curr1,curr2,amount)
+# _convert = _conversion.convert()
+
+# if(curr1 == "not found" or curr2 == "not found"):
+#     print(f"{country1} and {country2} not found.")
+#     print("Enter again: ")
+#     print()
+#     main()
     
-    if(curr1 == "not found"):
-        print(f"{country1} not found.")
-        print("Enter again: ")
-        print()
-        main()
-    
-    if(curr2 == "not found"):
-        print(f"{country2} not found.")
-        print("Enter again: ")
-        print()
-        main()
 
-    if(_convert == "curr1 not found"):
-        print(f"{country1} not found, ({country2} might me available).")
-        print("Enter again: ")
-        print()
-        main()
-    
-    if(_convert == "curr2 not found"):
-        print(f"{country2} not found.")
-        print("Enter again: ")
-        print()
-        main()
+# if(curr1 == "not found"):
+#     print(f"{country1} not found.")
+#     print("Enter again: ")
+#     print()
+#     main()
 
-    converted_amount = _convert * amount
-    print(f"The conversion rate from {country1} to {country2} is 1{curr1} = {_convert}{curr2}")
-    print(f"{amount}{curr1} = {converted_amount:.2f}{curr2}")
+# if(curr2 == "not found"):
+#     print(f"{country2} not found.")
+#     print("Enter again: ")
+#     print()
+#     main()
+
+# if(_convert == "curr1 not found"):
+#     print(f"{country1} not found, ({country2} might me available).")
+#     print("Enter again: ")
+#     print()
+#     main()
+
+# if(_convert == "curr2 not found"):
+#     print(f"{country2} not found.")
+#     print("Enter again: ")
+#     print()
+#     main()
+
+# converted_amount = _convert * amount
+# print(f"The conversion rate from {country1} to {country2} is 1{curr1} = {_convert}{curr2}")
+# print(f"{amount}{curr1} = {converted_amount:.2f}{curr2}")
+
+app = create_app()
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
+
